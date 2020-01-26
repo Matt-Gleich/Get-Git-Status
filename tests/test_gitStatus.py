03d6323077f9f45b474be03332624d0dcfe07544
Matthew Gleich <matthewgleich@gmail.com>
2020-01-26 17:14:28 -0500

    Create tests for test_untrackedFiles method

--- tests/test_gitStatus.py
+++ tests/test_gitStatus.py
@@ -1,11 +1,34 @@
 import sys
+import subprocess
 import os
 sys.path.append("../gitStatus")
-os.chdir("../gitStatus")
 
 from main import gitStatus
 
 def test_untrackedFiles():
+    # ONE UNTRACKED FILE
+    os.chdir("../gitStatus")
     os.system("touch test.txt")
-    print(gitStatus(os.getcwd()).untrackedFiles())
-    assert gitStatus(os.getcwd()).untrackedFiles() == True
+    os.chdir("..")
+    oneFileResult = gitStatus(os.getcwd()).untrackedFiles()
+    os.chdir("gitStatus")
+    os.system("rm test.txt")
+    os.chdir("..")
+    assert oneFileResult == ['gitStatus/test.txt']
+    # TWO UNTRACKED FILES
+    os.system("touch test1.txt")
+    os.chdir("gitStatus")
+    os.system("touch test2.txt")
+    os.chdir("..")
+    twoFilesResult = gitStatus(os.getcwd()).untrackedFiles()
+    os.system("rm test1.txt")
+    os.chdir("gitStatus")
+    os.system("rm test2.txt")
+    os.chdir("..")
+    assert twoFilesResult == ['gitStatus/test2.txt', 'test1.txt']
+    # UNTRACKED FILES WITH TRACKED FILE CHANGES
+    os.system("touch test.txt")
+    os.system("rm LICENSE.md")
+    changesFilesResult = gitStatus(os.getcwd()).untrackedFiles()
+    os.system("rm test.txt")
+    assert changesFilesResult == ["test.txt"]
