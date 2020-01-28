b378ff0b8051890dc5bcd293538c08988239dceb
Matthew Gleich <matthewgleich@gmail.com>
2020-01-27 22:24:19 -0500

    Update main.py

--- gitStatus/main.py
+++ gitStatus/main.py
@@ -1,13 +1,14 @@
 import os
 import subprocess
 
+
 def getSectionFiles(sectionIdentifier, command):
     """Get the files for a certain part of the git status command
-    
+
     Arguments:
         sectionIdentifier {str} -- The string that identifies the file list section
         command {list} -- The git status command ran
-    
+
     Returns:
         list -- List of files if any
     """
@@ -24,17 +25,18 @@ def getSectionFiles(sectionIdentifier, command):
         elif line == "":
             blankLineCount += 1
     return files
-    
+
+
 def matchFileAndStatus(fileList):
     """Match a file with it current status
-    
+
     Arguments:
         fileList {list} -- List of all files read from the git status command
-    
+
     Returns:
         dict -- Matched files
     """
-    matchedFiles = {}    
+    matchedFiles = {}
     for file in fileList:
         status = file.split(":")[0]
         file = file.split(":")[-1].strip()
@@ -48,17 +50,18 @@ class gitStatus():
 
     def __init__(self, repoPath):
         """Create an object for the repo
-        
+
         Arguments:
             repoPath {str} -- Full path to the repo
         """
         self.path = repoPath
         os.chdir(self.path)
-        self.command = subprocess.run(["git", "status"], stdout=subprocess.PIPE).stdout.decode('utf-8').split("\n")
+        self.command = subprocess.run(
+            ["git", "status"], stdout=subprocess.PIPE).stdout.decode('utf-8').split("\n")
 
     def untrackedFiles(self):
         """Get a list of all the untrackedFiles for the repo
-        
+
         Returns:
             list -- List of all the files
         """
@@ -66,14 +69,13 @@ class gitStatus():
 
     def unstagedFiles(self):
         """Get a list of all the unstaged files for the repo
-        
+
         Returns:
             list -- List of all the files
         """
         files = getSectionFiles("Changes not staged for commit:", self.command)
         return matchFileAndStatus(files)
-    
+
     def stagedFiles(self):
         files = getSectionFiles("Changes to be committed:", self.command)
-        print(files)
         return matchFileAndStatus(files)
